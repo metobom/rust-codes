c416c6c5c21fc20f6af75263261dc306803af37e
metobom <46991761+metobom@users.noreply.github.com>
2021-04-11 16:55:26 +0300

    Add files via upload

---
+++ hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77.d
@@ -0,0 +1,5 @@
+/home/m3/meto_ws/teknotam/rust-env/hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77: src/main.rs
+
+/home/m3/meto_ws/teknotam/rust-env/hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/m3/meto_ws/teknotam/rust-env/hello-rust/target/debug/hello-rust: /home/m3/meto_ws/teknotam/rust-env/hello-rust/src/main.rs
