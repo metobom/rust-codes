3ba24ee529c1c2df8662fd1866937ac7803f1b64
metobom <46991761+metobom@users.noreply.github.com>
2022-03-28 15:04:07 +0300

    Delete hello-rust directory

--- hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77.d
+++
@@ -1,5 +0,0 @@
-/home/m3/meto_ws/teknotam/rust-env/hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77: src/main.rs
-
-/home/m3/meto_ws/teknotam/rust-env/hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77.d: src/main.rs
-
-src/main.rs:
--- hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/m3/meto_ws/teknotam/rust-env/hello-rust/target/debug/hello-rust: /home/m3/meto_ws/teknotam/rust-env/hello-rust/src/main.rs
